5da2bd3f42a4f201ec7a58ccd24e79f1e9aa640f
Dvdandrades <dvdandrades@gmail.com>
2026-03-05 09:34:59 +0100

    add schemas

---
+++ shortener_app/main.py
@@ -0,0 +1,19 @@
+import validators
+
+from fastapi import FastAPI, HTTPException
+from . import schemas
+
+def raise_bad_request(message: str):
+    raise HTTPException(status_code=400, detail=message)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return "Welcome to the URL Shortener API!"
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
