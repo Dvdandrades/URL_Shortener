369ae5f91a44644944726ebf4e75654dafec8c27
Dvdandrades <dvdandrades@gmail.com>
2026-03-07 10:19:07 +0100

    add delete url

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -52,7 +52,7 @@ def forward_to_target(url_key: str, request: Request, db: Session = Depends(get_
         raise_not_found(request)
 
 
-@app.get("/{secret_key}", name="administration info", response_model=schemas.URLInfo)
+@app.get("/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo)
 def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
     if db_url := crud.get_db_url_by_secret_key(db=db, secret_key=secret_key):
         return get_admin_info(db_url=db_url)
@@ -67,3 +67,11 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url = crud.create_db_url(db=db, url=url)
 
     return get_admin_info(db_url=db_url)
+
+@app.delete("/admin/{secret_key}")
+def delete_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.deactivate_db_url_by_secret_key(db=db, secret_key=secret_key):
+        message = f"Successfully deleted shortened URL for '{db_url.target_url}'"
+        return {"detail": message}
+    else:
+        raise_not_found(request)
